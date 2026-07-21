b5a53020d402c2278ef8e622a526f11c9b8e0c6b
Paul Swan <prswan@gmail.com>
2026-07-21 15:03:36 -0700

    Disconnect CDP-C305 pending a 3rd attempt to set the FIL voltage correctly.

--- libraries/SonyCDPC305/SonyCDPC305Layout.cpp
+++ libraries/SonyCDPC305/SonyCDPC305Layout.cpp
@@ -29,7 +29,7 @@ Data
 ----
 I can't remember why there were no live measurements, possibly the player died on the bench.
 From the service manual:
- - VFIL: 3.9 VAC
+ - VFIL: 3.9 VAC ** SEE TODO **
  - Vbb: -32  VDC
 
 Onkyo HT-R340 NPT-1516D Transformer
@@ -39,6 +39,12 @@ Onkyo HT-R340 NPT-1516D Transformer
  - VFIL @ VFD            2x 1R  : 3.25
 
  Display seems a little on the dim side.
+
+ TODO: The filament is not directly visible but from the slit at the back they are very bright red over voltage.
+  - The service manual shows 2 x 8R2 resistors in series with "F"
+  - I can't find where the 3.9 VAC stated above came from in the service manual, I suspect this is an error
+    in the XLS.
+  - Unplugged for now to swap out the 1R FIL resistors.
 */
 
 static const UINT8 s_manufacturer[] PROGMEM = "SONY";
